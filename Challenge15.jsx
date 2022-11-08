31fe2c3cebf56511d5a023d8f22f2e551ed06ea4
Gonzalo Ordoñez <114304150+GonzaloA17@users.noreply.github.com>
2022-11-08 09:31:05 -0300

    Add files via upload

---
+++ Challenge15.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+
+//ejercicio 1//
+/*
+class Car extends React.Component {
+   render (){
+    return <h2>Soy un auto</h2>
+    }   
+}
+
+export default Car; 
+*/
+
+//Ejercicio 2//
+/*
+function Car() {
+    return <h2>Hola Soy un Auto!</h2>;
+  }
+export default Car;
+  */
+
+//Eje 3//
+
+/*
+const root = ReactDOM.createRoot(
+    document.getElementById('root')
+  );
+  const element = <h1>Hola Soy un Auto!</h1>;
+  root.render(element);
+
+*/
+
+//Eje 4//
+/*
+function Car(props) {
+    return <h2>I am a { props.brand.name }!</h2>;
+  }
+
+*/
+
+//Eje5//
+
+/*
+
+function Car(){
+    <h2>Soy un Auto!</h2>
+    
+}
+
+function Garage (){
+    
+    return (
+        <>
+        <h1>Quien sos vos?</h1>
+        <Car></Car>
+        </>
+    )
+
+    
+
+
+}*/
